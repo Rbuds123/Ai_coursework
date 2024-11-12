a6ba317d983bc4bb65ba409dab443579b730e753
Whoosh12 <toerag9331@gmail.com>
2024-11-12 09:30:44 +0000

    task 4

---
+++ task_4.py
@@ -0,0 +1,8 @@
+import numpy as np
+import random
+
+cut = [fair, good, very good, premium, ideal] #cut of the diamond
+colour = [D, E, F, G, H, I, J] #colour of the diamond, D is the best, J is the worst
+clarity = [I1, SI2, SI1, VS2, VS1, VVS2, VVS1, IF] #how clear the diamond is, I1 is the worst, IF is the best
+x = []
+table = [] #integers
