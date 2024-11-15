e073f3fb85b0e95d3c4f84127b54efc39f7842ab
Whoosh12 <toerag9331@gmail.com>
2024-11-15 13:40:43 +0000

    Task 4 - population generation and weight generation

--- task_4.js
+++ task_4.js
@@ -1,6 +1,7 @@
 // import csv file, convert to json, 1-50k, each variable named, range between all valuesin each variable, give weight based on value of variable, given each diamond randomly, adjust weights and bias based on error
 
-const maxcarat = 545.65;
+const minPrice = 200;
+const maxPrice = 30000;
 const csv = require('csv-parser')
 const fs = require('fs')
 const results = {diamonds: []};
@@ -9,17 +10,45 @@ const population = []
 //cant be a binary string for the population
 const population_size = 50;
 const generations = 1000;
-const crossover_rate = 0.8;
-const mutation_rate = 0.01;
 
-const rand = Math.random()*545.65
-console.log(rand.toFixed(2));
+const cutArray = ['Fair', 'Good', 'Very Good', 'Premium', 'Ideal'];
+const colourArray = ['J', 'I', 'H', 'G', 'F', 'E', 'D'];
+const clarityArray = ['I1', 'SI2', 'SI1', 'VS2', 'VS1', 'VVS2', 'VVS1', 'IF'];
 
-function generate_population(population_size){
-  for(let i = 0; i < population_size; i++) {
-    population[i] = {carat: Math.round(Math.random()*545.65), cut: }
+
+function diamond_NN() {
+  const derivativeArr = []; //initialise the derivative array
+  function generate_population(population_size){
+    for(let i = 0; i < population_size; i++) {
+      const randCarat = Math.random()*5.01;
+      const randX = Math.random()*10.74;
+      const randY = Math.random()*58.9;
+      const randZ = Math.random()*31.8;
+      const randDepth = 2*randZ/(randX+randY);
+      const randTable = (Math.random()*52)+43;
+      population[i] = {carat: randCarat.toFixed(2), cut: cutArray[Math.floor(Math.random()*5)], colour: colourArray[Math.floor(Math.random()*7)], clarity: clarityArray[Math.floor(Math.random()*8)], depth: randDepth.toFixed(2), x: randX.toFixed(2), y: randY.toFixed(2), z: randZ.toFixed(2), table: randTable.toFixed(1)};
+    }
+    console.log(population);
   }
+  
+  //make the layers
+  //random weights at the start, backpropogation then changes the weights
+  function init(X, HL, Y){
+    const L = [X, HL, Y];
+    const W = [];
+    console.log(L);
+    for(let i = 0; i < L.length - 1; i++){
+      const w=[Math.random(), Math.random()]
+      W.push(w);
+    }
+    // for(let i = 0; i < L.length - 1; i++){
+    //   const d = [0, 0];
+    //   derivativeArr.push(d);
+    // }
+  }
+
 }
+
 
 function csv_Parse() {
   fs.createReadStream('data.csv')
@@ -31,4 +60,6 @@ function csv_Parse() {
   });
 }
 
+// generate_population(population_size);
 csv_Parse();
+init(2, [2,2], 2);
