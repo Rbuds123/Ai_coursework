be3ce3c335a9552d4c6fc22fe53eaa4de7e3b160
Whoosh12 <toerag9331@gmail.com>
2024-11-15 09:36:09 +0000

    task 4 stuff

--- task_4.py
+++ task_4.py
@@ -1,8 +1,15 @@
 import numpy as np
 import random
 
-cut = [fair, good, very good, premium, ideal] #cut of the diamond
-colour = [D, E, F, G, H, I, J] #colour of the diamond, D is the best, J is the worst
-clarity = [I1, SI2, SI1, VS2, VS1, VVS2, VVS1, IF] #how clear the diamond is, I1 is the worst, IF is the best
-x = []
-table = [] #integers
+price = range(326, 18823, 1)
+carat = range(20, 501, 1) #most important
+cut = [1, 2, 3, 4, 5] #cut of the diamond
+colour = [7, 6, 5, 4, 3, 2, 1] #colour of the diamond
+clarity = [1, 2, 3, 4, 5, 6, 7, 8] #how clear the diamond is
+x = range(0, 1074, 1)
+y = range(0, 5890, 1)
+z = range(0, 3180, 1)
+table = range(43, 95, 1) #integers
+depth = z/((x+y)*2)
+
+print(depth)
